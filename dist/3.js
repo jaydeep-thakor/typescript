bd244cf14c4d79e5ce182a2d573185ebb5215128
jaydeep-thakor <imjaydeepcreation@gmail.com>
2025-12-12 22:04:43 +0530

    Initial commit: TypeScript code

---
+++ dist/3.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    // return "Hello "+name
+    return `Hello ${name}`;
+}
+const myName = "Jaydeep";
+console.log(greet(myName));
+//# sourceMappingURL=3.js.map
