3d77204fb7936f38a538a2454177a0fbf2af5fe0
jaydeep-thakor <imjaydeepcreation@gmail.com>
2025-12-12 23:25:18 +0530

    5th tutorial code added

---
+++ src/5-union_any.ts
@@ -0,0 +1,25 @@
+// union types
+let subscribers: number | string = "10M";
+
+let apiRequestStatus: 'pending' | 'success' | 'error' = "pending";
+// apiRequestStatus = "done" // error
+
+let airlineSeat: 'aisle' | 'window' | 'middle' = "middle";
+airlineSeat = "window";
+
+// any (avoid any)
+
+let orders = ["10","25","35","47"];
+let currentOrder: string | undefined;
+
+for(let allOrders of orders){
+    if(allOrders === "25"){
+        currentOrder = allOrders;
+        break;
+    }
+};
+
+console.log(currentOrder);
+
+
+
