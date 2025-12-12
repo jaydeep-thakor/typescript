f473d7e6b00df20dbc9fc806fa5fb89d5cc2bba7
jaydeep-thakor <imjaydeepcreation@gmail.com>
2025-12-12 22:52:53 +0530

    4th tutorial code

---
+++ src/4.ts
@@ -0,0 +1,7 @@
+// Annotations (Type Annotations)
+// Explicit type information you add to variables, functions, or parameters to tell TypeScript what type they should be.
+// Example: let age: number = 20;
+
+// Inference (Type Inference)
+// When TypeScript automatically figures out the type without you writing it.
+// Example: let age = 20; → TypeScript infers age is a number.
